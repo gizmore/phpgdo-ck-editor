c53d8b2f69dbd3f893967737764d8c9167efc1e1
gizmore <gizmore@wechall.net>
2022-10-21 06:23:45 +0200

    '7.0.1-r1669 fineprint and kceditor'

--- Module_CKEditor.php
+++ Module_CKEditor.php
@@ -7,41 +7,54 @@ use GDO\UI\GDT_Message;
 
 /**
  * CKEditor bindings.
- * 
+ *
  * @author gizmore
  * @version 6.10.4
  * @since 6.8.0
  */
 final class Module_CKEditor extends GDO_Module
 {
-    public function getDependencies() : array { return ['JQuery']; }
-    public function thirdPartyFolders() : array { return ['ckeditor5/']; }
-    public function onLoadLanguage() : void { $this->loadLanguage('lang/ckeditor'); }
-    public function getClasses() : array
-    {
-        return [
-            GDO_CKFile::class,
-        ];
-    }
-    
-    ############
-    ### Init ###
-    ############
-    public function onModuleInit()
-    {
-        GDT_Message::$EDITOR_NAME = 'CKEditor';
-    }
-    
-    public function onIncludeScripts() : void
-    {
-        $this->addJS('ckeditor5/ckeditor.js');
-        $iso = strtolower(GDO_Language::current()->getISO());
-        if ($iso != 'en')
-        {
-            $this->addJS('ckeditor5/translations/'.$iso.'.js');
-        }
-        
-        $this->addJS('js/gdo6-ckeditor.js');
-    }
-    
+
+	public function getDependencies(): array
+	{
+		return [
+			'JQuery'
+		];
+	}
+
+	public function thirdPartyFolders(): array
+	{
+		return [
+			'ckeditor5/'
+		];
+	}
+
+	public function onLoadLanguage(): void
+	{
+		$this->loadLanguage('lang/ckeditor');
+	}
+
+	public function getClasses(): array
+	{
+		return [
+			GDO_CKFile::class,
+		];
+	}
+
+	# ###########
+	# ## Init ###
+	# ###########
+	public function onModuleInit()
+	{
+		GDT_Message::$EDITOR_NAME = 'CKEditor';
+	}
+
+	public function onIncludeScripts(): void
+	{
+		$iso = GDO_Language::current()->getISO();
+		$this->addJS('ckeditor5/build/ckeditor.js');
+		$this->addJS('ckeditor5/build/translations/' . $iso . '.js');
+		$this->addJS('js/gdo7-ckeditor.js');
+	}
+
 }
---
+++ js/gdo7-ckeditor.js
@@ -0,0 +1,26 @@
+"use strict";
+let nodes = document.querySelectorAll('div.wysiwyg.gdt-editor-ckeditor textarea');
+for (let i in nodes) {
+	let node = nodes[i];
+	createEditor(node);
+}
+
+function createEditor(node) {
+	let uploadUrl = window.GDO.href('CKEditor', 'Upload');
+	window.ClassicEditor.create(node, {
+	    simpleUpload: {
+            // The URL that the images are uploaded to.
+            uploadUrl: uploadUrl,
+
+            // Enable the XMLHttpRequest.withCredentials property.
+            withCredentials: true,
+
+            // Headers sent along with the XMLHttpRequest to the upload server.
+//            headers: {
+//                'X-CSRF-TOKEN': 'CSRF-Token',
+//                Authorization: 'Bearer <JSON Web Token>'
+//            }
+        }
+	
+	});
+}
